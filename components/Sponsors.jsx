db61eb3d50f93068c65c02e8c014aadea8de5efb
PVSekhar1234 <cse210001051@iiti.ac.in>
2022-12-24 20:09:21 +0530

    removed height issue in sponsers section

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -60,12 +60,13 @@ function Sponsors() {
   return (
     <motion.Reactdiv
       id="sponsors"
-      className="z-0 flex items-center justify-center md:h-[1000px] mb-4 lg:mb-0 sm:h-[1500px] h-[2500px] w-screen bg-[#faf3ea]"
+      className="z-0 flex items-center justify-center w-screen"
+      // className="z-0 flex items-center justify-center md:h-[900px] sm:h-[1500px] h-[1500px] w-screen"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <div className="flex flex-col h-screen w-screen w-full mt-0 md:px-4 mt-0">
+      <div className="flex flex-col w-screen w-full pb-10 mt-0 top-0 md:px-4 mt-0">
         <div className="flex w-full z-50">
           <h1
             style={{
@@ -144,7 +145,7 @@ function Sponsors() {
               </div>
               <div className="sponsor_text">
                 <h1 className="uppercase text-center text-[#111b53]">
-                  Media sponsor <br></br> Coca Cola
+                  associate sponsor <br></br> Coca Cola
                 </h1>
               </div>
             </div>
