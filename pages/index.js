2674941abba53b55935a5840f0e3757fa5b1796e
kr-2003 <kumar.kr.abhinav@gmail.com>
2022-12-25 11:37:26 +0530

    hehe

--- pages/index.js
+++ pages/index.js
@@ -9,76 +9,74 @@ import Particle from "../components/Particle";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Progress from "../components/Progress";
-import window from 'global'
+// import window from 'global'
 
 function home() {
-  console.log(window.innerWidth)
+  // console.log(window.innerWidth);
   // const [is_desktop,setType]=useState(false)
-  if(window.innerWidth>760){
-    return (
-      <div className="overflow-x-hidden">
-        <motion.div
-          initial={{ opacity: 0.4 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8 }}
-          exit={{ opacity: 2 }}
-          >
-            <Particle></Particle>
-          {/* <DeviceIdentifier isDesktop={true}>
+  // if(window.innerWidth>760){
+  //   return (
+  //     <div className="overflow-x-hidden">
+  //       <motion.div
+  //         initial={{ opacity: 0.4 }}
+  //         animate={{ opacity: 1 }}
+  //         transition={{ duration: 0.8 }}
+  //         exit={{ opacity: 2 }}
+  //         >
+  //           <Particle></Particle>
+  //         {/* <DeviceIdentifier isDesktop={true}>
+  //           <Particle></Particle>
+  //         </DeviceIdentifier> */}
+  //         {/* <CustomView condition={is_desktop}>
+  //           <Particle></Particle>
+  //         </CustomView> */}
+  //         {/* if(is_desktop){
+  //           <Particle></Particle>
+  //         } */}
+  //         {/* <BrowserView>
+  //           <Particle></Particle>
+  //         </BrowserView> */}
+  //         <Navbar></Navbar>
+  //         <HomePage></HomePage>
+  //         <About></About>
+  //         <Events></Events>
+  //         <Sponsors></Sponsors>
+  //         <Footer></Footer>
+  //       </motion.div>
+  //     </div>
+  //   );
+
+  // }else{
+  return (
+    <div className="overflow-x-hidden">
+      <motion.div
+        initial={{ opacity: 0.4 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        exit={{ opacity: 2 }}
+      >
+        {/* <Particle></Particle> */}
+        {/* <DeviceIdentifier isDesktop={true}>
             <Particle></Particle>
           </DeviceIdentifier> */}
-          {/* <CustomView condition={is_desktop}>
+        {/* <CustomView condition={is_desktop}>
             <Particle></Particle>
           </CustomView> */}
-          {/* if(is_desktop){
+        {/* if(is_desktop){
             <Particle></Particle>
           } */}
-          {/* <BrowserView>
+        {/* <BrowserView>
             <Particle></Particle>
           </BrowserView> */}
-          <Navbar></Navbar>
-          <HomePage></HomePage>
-          <About></About>
-          <Events></Events>
-          <Sponsors></Sponsors>
-          <Footer></Footer>
-        </motion.div>
-      </div>
-    );
-    
-  }else{
-    return (
-      <div className="overflow-x-hidden">
-        <motion.div
-          initial={{ opacity: 0.4 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8 }}
-          exit={{ opacity: 2 }}
-          >
-            {/* <Particle></Particle> */}
-          {/* <DeviceIdentifier isDesktop={true}>
-            <Particle></Particle>
-          </DeviceIdentifier> */}
-          {/* <CustomView condition={is_desktop}>
-            <Particle></Particle>
-          </CustomView> */}
-          {/* if(is_desktop){
-            <Particle></Particle>
-          } */}
-          {/* <BrowserView>
-            <Particle></Particle>
-          </BrowserView> */}
-          <Navbar></Navbar>
-          <HomePage></HomePage>
-          <About></About>
-          <Events></Events>
-          <Sponsors></Sponsors>
-          <Footer></Footer>
-        </motion.div>
-      </div>
-    );
-    
-  }
+        <Navbar></Navbar>
+        <HomePage></HomePage>
+        <About></About>
+        <Events></Events>
+        <Sponsors></Sponsors>
+        <Footer></Footer>
+      </motion.div>
+    </div>
+  );
 }
 
 export default home;
